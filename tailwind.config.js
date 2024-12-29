618dbb11407d7560bb8092c831a3fe91bcc5dc54
Migueldev <contatomigueldev@gmail.com>
2024-12-28 21:49:14 -0300

    Ajustes finais nos arquivos de configuração

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html, js}"],
   theme: {
+    colors: {
+      "strong-cyan": "hsl(172, 67%, 45%)",
+      "very-dark-cyan": "hsl(183, 100%, 15%)",
+      "dark-grayish-cyan": "hsl(186, 14%, 43%)",
+      "light-grayish-cyan": "hsl(185, 41%, 84%)",
+      "very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontFamily: {
+      mono: ["Space Mono", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
